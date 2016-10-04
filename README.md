ea7e06a5960d4d8a87d57db100e261fe8967e488
foukLor <vitaminotar@yandex.com>
2016-10-04 19:06:44 +0300

    scrit for searching trends on github.com

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # 9_github_trending
+
+
+This script is using to get trends from github.com 
+
+You can see trend repositories and issues of these repos
+
+## Usage:
+
+```sh
+python 
+```
+
+please use python version 3.5
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,30 @@
+import requests
+from datetime import date, timedelta
 
 
 def get_trending_repositories(top_size):
-    pass
+    date_week_ago = date.today() - timedelta(days=7)
+    url = "https://api.github.com/search/repositories?"\
+        "q=+created:>={0}&sort=stars&order=desc".format(date_week_ago)
+    response = requests.get(url)
+    return response.json()['items'][:top_size]
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    pass
+    url = 'https://api.github.com/repos/{0}/{1}/issues'\
+        .format(repo_owner, repo_name)
+    response = requests.get(url)
+    return len(response.json())
+
+
+def pretty_print(repositories):
+    for repo in repositories:
+        print("\nName: {0}".format(repo['name']))
+        print("Url: {0}".format(repo['html_url']))
+        print("Count of issues {0}".format(
+            get_open_issues_amount(repo['owner']['login'], repo['name'])))
+
 
 if __name__ == '__main__':
-    pass
+    trends_rep = get_trending_repositories(10)
+    pretty_print(trends_rep)
